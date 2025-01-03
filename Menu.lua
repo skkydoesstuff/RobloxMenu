f456210b631989e937428ed8bc7f92cd49ba46aa
skky <petesky26@gmail.com>
2025-01-03 03:07:00 -0500

    fixed menu.lua

--- Menu.lua
+++ Menu.lua
@@ -1,5 +1,5 @@
 local library = loadstring(game:HttpGet("https://raw.githubusercontent.com/skkydoesstuff/RobloxMenu/refs/heads/main/SplixCustom.lua"))()
-local window = library:new({textsize = 13.5,font = Enum.Font.RobotoMono,name = "YES",color = Color3.fromRGB(225,58,81)})
+local window = library:new({textsize = 13.5,font = Enum.Font.RobotoMono,name = "Skkys Gui",color = Color3.fromRGB(225,58,81)})
 local tab = window:page({name = "General"})
 
 local player = game.Players.LocalPlayer
@@ -8,6 +8,8 @@ local hrp = char:WaitForChild("HumanoidRootPart")
 local h = char:WaitForChild("Humanoid")
 
 local LocalPlayerSection = tab:section({name = "Local Player",side = "left",size = 250})
+local LocalPlayerSection2 = tab:section({name = "Local Player2",side = "left",size = 250})
+local LocalPlayerSection3 = tab:section({name = "Local Player3",side = "left",size = 250})
 
 local defaultWalkSpeed = h.WalkSpeed
 local defaultJumpHeight = h.JumpHeight
