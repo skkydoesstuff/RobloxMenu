fddd0f1322df21c7dee6e742918ad2591c05a6cc
skky <petesky26@gmail.com>
2025-01-03 03:10:25 -0500

    testing

--- Menu.lua
+++ Menu.lua
@@ -1,5 +1,10 @@
 local library = loadstring(game:HttpGet("https://raw.githubusercontent.com/skkydoesstuff/RobloxMenu/refs/heads/main/SplixCustom.lua"))()
 local window = library:new({textsize = 13.5,font = Enum.Font.RobotoMono,name = "Skkys Gui",color = Color3.fromRGB(225,58,81), size = Vector2.new(600, 1000)})
+
+if window.outline then
+    window.outline.Size = UDim2.new(0, 600, 0, 1000)
+end
+
 local tab = window:page({name = "General"})
 
 local player = game.Players.LocalPlayer
